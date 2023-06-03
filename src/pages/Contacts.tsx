acfd476c88ff9ca6150877d49e3da34e0d40178a
limitr <kakashihotakimail.ru@gmail.com>
2023-06-03 17:57:33 +0300

    feat: add yandex map to contact page

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
-
+import { YMaps, Map } from '@pbe/react-yandex-maps'
 export function Contacts() {
-  return <div>Контакты</div>
+  return (
+    <>
+      <div>Контакты</div>
+      <YMaps>
+        <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+      </YMaps>
+    </>
+  )
 }
