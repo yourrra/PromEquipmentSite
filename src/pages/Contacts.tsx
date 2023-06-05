6cbebb91cb1ff0c49573554d3b0db3cc525c9b9b
yourrra <yuri15616@gmailcom>
2023-06-05 17:44:07 +0300

    feat: add page contacts

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -4,13 +4,69 @@ export function Contacts() {
   return (
     <div className="wrapper">
       <div className={classes.contacts__wrapper}>
-        <h1>Контакты</h1>
-        <p>
-          Компания «Агро-инжиниринг ЮГ» предлагает комплексные решения в сфере
-          хранения и переработки зерна. Современные технологии и многолетний
-          опыт специалистов компании позволяют создавать эффективные и
-          качественные продукты точно отвечающие потребностям наших клиентов.
-        </p>
+        <h1 className={classes.contacts__title}>Контакты</h1>
+        <div className="title__line"></div>
+        <div className={classes.contacts__contactblock}>
+          <h2 className={classes.contacts__topic}>Рабочие телефоны</h2>
+          <div className={classes.contacts__cardblock}>
+            <div className={classes.contacts__card}>
+              <h3 className={classes.contacts__card_title}>Отдел продаж</h3>
+              <div className={classes.contacts__card_phone}>
+                <p className={classes.contacts__card_topic}>Телефон:</p>
+                <a
+                  className={classes.contacts__card_link}
+                  href="tel:8 800 866 86 86"
+                >
+                  8 800 866 86 86
+                </a>
+              </div>
+              <div>
+                <p className={classes.contacts__card_topic}>Почта:</p>
+                <a
+                  className={classes.contacts__card_link}
+                  href="mailto:example@gmail.com"
+                >
+                  example@gmail.co
+                </a>
+              </div>
+            </div>
+            <div className={classes.contacts__card}>
+              <h3 className={classes.contacts__card_title}>Приемная</h3>
+              <div>
+                <p className={classes.contacts__card_topic}>Почта:</p>
+                <a
+                  className={classes.contacts__card_link}
+                  href="mailto:example@gmail.com"
+                >
+                  example@gmail.co
+                </a>
+              </div>
+            </div>
+            <div className={classes.contacts__card}>
+              <h3 className={classes.contacts__card_title}>Отдел снабжения</h3>
+              <div className={classes.contacts__card_phone}>
+                <p className={classes.contacts__card_topic}>Телефон:</p>
+                <a
+                  className={classes.contacts__card_link}
+                  href="tel:8 800 866 86 86"
+                >
+                  8 800 866 86 86
+                </a>
+              </div>
+              <div>
+                <p className={classes.contacts__card_topic}>Почта:</p>
+                <a
+                  className={classes.contacts__card_link}
+                  href="mailto:example@gmail.com"
+                >
+                  example@gmail.co
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h2 className={classes.contacts__topic}>Наш адрес</h2>
+        <h2 className={classes.contacts__topic}>Остались вопросы?</h2>
       </div>
     </div>
   )
